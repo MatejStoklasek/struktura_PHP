7e6710ecaff37a1e5d9e280574b9d89ddb917274
matej.stoklasek <matej.stoklasek@orbiszlin.cz>
2025-11-28 13:18:39 +0100

    commit

---
+++ components/functions2.php
@@ -0,0 +1,61 @@
+<?php
+
+/*$a = 2;
+$b = 4;
+
+
+function pow2(&$x, $num)
+{
+
+    $x = $x ** $num;
+    return $x;
+
+}
+echo $a . "<br><br>";
+echo pow2($a, $b);
+// $a = pow2($a, $b);
+echo "<br><br>" . $a;
+*/
+
+// /** @var int|string $a asdfasdf */
+
+/*
+$a = "string";
+
+//echo str_repeat($a, 5);
+
+
+function xx(string $a)
+{
+    echo $a;
+}
+
+echo xx($a);
+*/
+/*
+/**
+ * titulek
+ *
+ * Popis funkce
+ *
+ * @param array<string> $arr
+ * @return void
+ */
+/*
+function printArr(array $arr)
+{
+    print_r($arr);
+}
+
+$pole = ['a','b','c'];
+
+printArr($pole);
+*/
+
+/** Konstanty */
+
+const BASE_URL = 'https://example.com/';
+
+//echo BASE_URL;
+
+echo __DIR__;
